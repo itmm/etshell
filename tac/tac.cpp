93b712bf8de4eb397fe0fefbbf20b6f9250a6b81
Timm Knape <t.knape@arz-darmstadt.de>
2026-01-15 11:24:57 +0100

    Logging zu tac hinzugefügt

--- tac/tac.cpp
+++ tac/tac.cpp
@@ -3,11 +3,12 @@
 #include "log/log.h"
 #include "ta/ta.h"
 
-static inline void process(ta::ostream& out, const char* path) {
+static inline void process(ta::ostream& out, ta::ostream& err, const char* path) {
 	std::ifstream f { path };
 	if (! f) { log_fatal("Kann Datei nicht öffnen", ""); }
 
 	out.open_next_file(path);
+	err.open_next_file(path);
 
 	int ch;
 	while ((ch = f.get()) >= 0) {
@@ -19,9 +20,10 @@ int main(int argc, const char *argv[]) {
 	try {
 		if (argc >= 2) {
 			ta::ostream out { std::cout };
+			ta::ostream err { std::cerr };
 			const char** paths = argv + 1;
 			for (int i = argc - 1; i; --i) {
-				process(out, *paths++);
+				process(out, err, *paths++);
 			}
 		}
 		return EXIT_SUCCESS;
